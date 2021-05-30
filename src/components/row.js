a936382905b91eed4fb74db35e1bb4b4c2a9a609
Anam Haque <palash2722@gmail.com>
2021-05-31 00:38:34 +1000

    Row1

--- src/components/row.js
+++ src/components/row.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './style/row.css';
 
-function Row(props) {
+function Row(setNotes) {
 	return(
-		<div key={"title" + props.index} className="body-row">
-			{props.index + 1}. <span>{props.value}</span>
-			<button onClick={props.onRowDelete}>X</button>
+		<div key={"title" + setNotes.index} className="body-row">
+			{setNotes.index + 1}. <span>{setNotes.value}</span>
+			<button onClick={setNotes.onRowDelete}>X</button>
 		</div>
 	);
 }
